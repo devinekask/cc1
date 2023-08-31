7caf3a7f34c3172a742a6fc7ea5f72581f10a781
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2023-08-31 12:48:42 +0200

    commit hook

---
+++ 01-basic-drawing/demos/frameloop/js/script.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500, 500);
+  //set the Frameloop to 30 frames per second
+  frameRate(30);
+}
+
+function draw() {
+  background(220) ;
+  ellipse(mouseX, mouseY, 100, 100);
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Creative code 1
