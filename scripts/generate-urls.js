7b54b64ae164ef8f7475da73117d93d6b7282592
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-09-07 15:27:58 +0200

    added astro

--- scripts/generate-urls.js
+++ scripts/generate-urls.js
@@ -1,7 +1,8 @@
 import fs from "fs/promises";
 import path from "path";
 
-const codesandbox_base_url = "https://codesandbox.io/s/github/devinekask/cc1/tree/main/";
+const codesandbox_base_url =
+  "https://codesandbox.io/s/github/devinekask/cc1/tree/main/";
 const github_base_url = "https://github.com/devinekask/cc1";
 
 const getUrls = async () => {
@@ -13,7 +14,6 @@ const getUrls = async () => {
     const files = await fs.readdir(currentDir);
 
     for (const file of files) {
-
       if (!file.match("^[0-9]")) {
         continue;
       }
@@ -24,14 +24,12 @@ const getUrls = async () => {
     }
 
     return urls;
-
   } catch (error) {
     console.error(`An error occurred while getting folders: ${error}`);
   }
 };
 
 async function addUrls(urls, file, currentDir, folderName) {
-
   const pathToFolder = path.join(currentDir, file, folderName);
 
   try {
@@ -42,30 +40,28 @@ async function addUrls(urls, file, currentDir, folderName) {
     urls[file][folderName] = [];
 
     for (const demoFolder of demoFolders) {
-      urls[file][folderName].push(
-        {
-          "name": demoFolder,
-          "sandbox": `${codesandbox_base_url}${file}/demos/${demoFolder}`,
-          "github": `${github_base_url}/tree/main/${file}/demos/${demoFolder}/js/script.js`,
-          "relativePath": `./${file}/${folderName}/${demoFolder}`
-        }
-      );
-    };
+      urls[file][folderName].push({
+        name: demoFolder,
+        sandbox: `${codesandbox_base_url}${file}/demos/${demoFolder}`,
+        github: `${github_base_url}/tree/main/${file}/demos/${demoFolder}/js/script.js`,
+        relativePath: `./${file}/${folderName}/${demoFolder}`,
+      });
+    }
   } catch (error) {
     console.error(`An error occurred while getting folders: ${error}`);
   }
-
 }
 
 const init = async () => {
-
   const urls = await getUrls();
   const jsonContent = JSON.stringify(urls);
   const jsonFileName = "urls.json";
 
   fs.writeFile(jsonFileName, jsonContent)
     .then(() => console.log(`URLs saved to ${jsonFileName} `))
-    .catch((error) => console.error(`An error occurred while writing JSON file: ${error} `));
+    .catch((error) =>
+      console.error(`An error occurred while writing JSON file: ${error} `),
+    );
 };
 
 init();
