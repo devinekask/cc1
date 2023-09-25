9f5588ffb0e09c9f39e38c991fd0a938489ff9bf
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-09-25 13:17:24 +0200

    starter files

---
+++ 04-random/exercises/start/02-pollock/js/script.js
@@ -0,0 +1,16 @@
+/*
+RGB Values
+227, 0, 34
+0, 123, 167
+204, 188, 0
+0, 168, 107
+237, 41, 57
+*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  ellipse(mouseX, mouseY, 50, 50);
+}
