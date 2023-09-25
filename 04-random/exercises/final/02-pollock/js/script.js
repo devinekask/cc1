a2907ad5b581d520cbc865d85c69cf4248e3953f
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-09-25 12:55:49 +0200

    fix pollock

--- 04-random/exercises/final/02-pollock/js/script.js
+++ 04-random/exercises/final/02-pollock/js/script.js
@@ -6,7 +6,7 @@ function setup() {
 function draw() {
   if (mouseIsPressed) {
     // Set a random color
-    const randomNumber = Math.ceil(random(1, 5));
+    const randomNumber = Math.ceil(random(0, 5));
     switch (randomNumber) {
       case 1:
         fill(227, 0, 34);
@@ -23,28 +23,12 @@ function draw() {
       case 5:
         fill(237, 41, 57);
         break;
-      case 6:
-        fill(204, 85, 0);
-        break;
-      case 7:
-        fill(255, 217, 0);
-        break;
-      case 8:
-        stroke(0, 45, 114);
-        break;
-      case 9:
-        stroke(115, 74, 18);
-        break;
-      case 10:
-        stroke(0, 150, 100);
-        break;
       default:
         break;
     }
 
-    // Random circle size
+    const randomSize = random(1, 20);
 
-    const randomSize = random(1, 35);
     noStroke();
     ellipse(mouseX, mouseY, randomSize);
   }
