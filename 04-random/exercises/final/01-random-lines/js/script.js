c774f9cb1d984fe69a59f8bbbcaaf778102bc6b1
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-09-25 10:58:10 +0200

    random lines

---
+++ 04-random/exercises/final/01-random-lines/js/script.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  const margin = 50;
+  const x1 = constrain(random(400), margin, width - margin);
+  const x2 = constrain(random(400), margin, width - margin);
+  const y1 = constrain(random(400), margin, height - margin);
+  const y2 = constrain(random(400), margin, height - margin);
+
+  stroke(255);
+  line(x1, y1, x2, y2);
+
+  noFill();
+  rect(margin, margin, width - 2 * margin, height - 2 * margin);
+
+  if (frameCount >= 30) {
+    noLoop();
+  }
+}
