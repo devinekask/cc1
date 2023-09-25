c69bf474f3dbf280e2f7a1cf3432ebf6eb6ae7cc
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-09-25 13:09:22 +0200

    pollock using floor

--- 04-random/exercises/final/02-pollock/js/script.js
+++ 04-random/exercises/final/02-pollock/js/script.js
@@ -5,22 +5,22 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    // Set a random color
-    const randomNumber = Math.ceil(random(0, 5));
+    const randomNumber = Math.floor(random(0, 5));
+
     switch (randomNumber) {
-      case 1:
+      case 0:
         fill(227, 0, 34);
         break;
-      case 2:
+      case 1:
         fill(0, 123, 167);
         break;
-      case 3:
+      case 2:
         fill(204, 188, 0);
         break;
-      case 4:
+      case 3:
         fill(0, 168, 107);
         break;
-      case 5:
+      case 4:
         fill(237, 41, 57);
         break;
       default:
