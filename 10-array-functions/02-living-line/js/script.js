a3361a2cea22de664be32f0cda700855b24e2cf4
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-11-05 19:16:07 +0100

    array functions- temp

---
+++ 10-array-functions/02-living-line/js/script.js
@@ -0,0 +1,33 @@
+const nPoints = 25;
+const points = [];
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(30);
+}
+
+function draw() {
+  background(0);
+
+  const newPoint = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  // Add new (last)
+  points.push(newPoint);
+
+  if (points.length > nPoints) {
+    points.shift();
+  }
+
+  // Loop through points & draw lines
+  noFill();
+  beginShape();
+  stroke(255);
+  strokeWeight(3);
+  points.forEach((point) => {
+    vertex(point.x, point.y);
+  });
+  endShape();
+}
