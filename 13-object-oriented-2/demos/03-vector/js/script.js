d4ecf1479344b72628851a43d106c3afd5c162a9
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2023-11-20 07:40:55 +0100

    createVector & add

---
+++ 13-object-oriented-2/demos/03-vector/js/script.js
@@ -0,0 +1,22 @@
+let positionX;
+let positionY;
+
+let step;
+
+function setup() {
+  createCanvas(400, 400);
+
+  frameRate(1);
+
+  position = createVector(10, 10);
+  step = createVector(0, 10);
+}
+
+function draw() {
+  background(0);
+
+  ellipse(position.x, position.y, 10);
+
+  position.x += step.x;
+  position.y += step.y;
+}
---
+++ 13-object-oriented-2/demos/04-add/js/arrow.js
@@ -0,0 +1,29 @@
+class Arrow {
+  constructor(x, y, size) {
+    this.location = createVector(x, y);
+    this.velocity = createVector(1, 1.5);
+    this.size = size;
+  }
+
+  run() {
+    this.display();
+    this.update();
+  }
+
+  update() {
+    this.location.add(this.velocity);
+  }
+
+  display() {
+    push();
+
+    translate(this.location.x, this.location.y);
+    stroke(255);
+    strokeWeight(4);
+    line(-this.size, 0, 0, 0);
+    line(-this.size / 4, -this.size / 4, 0, 0);
+    line(-this.size / 4, this.size / 4, 0, 0);
+
+    pop();
+  }
+}
