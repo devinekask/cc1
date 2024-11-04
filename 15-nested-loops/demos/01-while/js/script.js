31e9afac02562379919480ffa22c3e94916f8142
vaernewijck <pieter-jan.vaernewijck@outlook.be>
2024-11-04 20:01:02 +0100

    do and do while demos

---
+++ 15-nested-loops/demos/01-while/js/script.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  // Draw 5 ellipses;
+  let i = 0;
+
+  while (i < 5) {
+    ellipse(25 + i * 50, 50, 50);
+    i++;
+  }
+
+  // As much ellipses as we can
+  let x = 50;
+  while (x <= width - 50) {
+    ellipse(x, height / 2, 50, 50);
+    x += 100;
+  }
+}
---
+++ 15-nested-loops/demos/02-do-while/js/script.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+  // Draw 5 ellipses using do while
+
+  let i = 0;
+  do {
+    ellipse(25 + i * 50, 100, 50);
+    i++;
+  } while (i < 5);
+
+}
